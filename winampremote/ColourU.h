347e796a0dc83fd61a42279fdebef9ac43019c73
Patrick Martin <patrickmmartin@gmail.com>
2011-11-13 15:30:11 +0000

    removing old files

--- winampremote/ColourU.h
+++
@@ -1,9 +0,0 @@
-//---------------------------------------------------------------------------
-#ifndef ColourUH
-#define ColourUH
-//---------------------------------------------------------------------------
-
-double periodic(double percent);
-TColor RainbowColour(int value);
-
-#endif
