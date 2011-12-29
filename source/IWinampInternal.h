bfd36765e0f6e47164c80bb0ff2fef2c2da7ab78
Patrick Martin <patrickmmartin@gmail.com>
2011-12-29 14:18:38 +0000

    adding initial revision of the internal abstraction

---
+++ source/IWinampInternal.h
@@ -0,0 +1,22 @@
+#ifndef IWINAMPINTERNAL_H
+#define IWINAMPINTERNAL_H
+
+// abstract interface wrapping the internal operations Winamp supplies
+// via the Windows messages oriented interface
+
+class IWinampInternal {
+
+  public:
+
+    virtual ~IWinampInternal() {}
+
+    void ExecuteMessage(Command);
+    void ExecuteStringMessage(char * CommandString, int Command);
+    int GetAmpInt(int Command, int Data);
+    char * GetAmpStr(int Command, int Data);
+    char * GetWinampVersion(void);
+    char * WinampVersion(int retval);
+
+};
+
+#endif // IWINAMPINTERNAL_H
