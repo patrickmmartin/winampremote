2d46330b225565814dd446450f758f6b5ca959e9
Patrick Martin <patrickmmartin@gmail.com>
2012-01-08 21:18:08 +0000

    related to #57  * adding simple whereis script  * updating minimal path script to include windows System

--- source/myenv.bat
+++ source/myenv.bat
@@ -1,1 +1,1 @@
-SET PATH=C:\CBuilder5\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files (x86)\doxygen\bin"
+SET PATH=C:\CBuilder5\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files (x86)\doxygen\bin";C:\Windows\System32
