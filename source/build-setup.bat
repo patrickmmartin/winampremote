c321e08003221536b34ef0cb573599f78b70cbd0
Patrick Martin <patrickmmartin@gmail.com>
2012-02-18 14:26:06 +0000

    env variable is not in scope at that point of the script

--- source/build-setup.bat
+++ source/build-setup.bat
@@ -5,8 +5,9 @@
 
 FOR /F "eol=; delims=, tokens=1,2,3,4" %%i IN (version.txt) DO (
 SET APPVER=%%i.%%j.%%k.%%l
-echo building installer for [%APPVER%]
 )
+
+echo building installer for [%APPVER%]
 
 C:\Windows\Microsoft.NET\Framework\v2.0.50727\MSBuild /nologo ..\setup\WinampRemote.wixproj  /t:%1
 
