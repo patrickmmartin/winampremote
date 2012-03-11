558cc39d18bb5733f666c7a6955b456eb175ebf7
Patrick Martin <patrickmmartin@gmail.com>
2012-03-11 11:46:45 +0000

    dual-OS support in the env batch file

--- source/myenv.bat
+++ source/myenv.bat
@@ -1,1 +1,7 @@
+@echo off
+IF "%ProgramFiles(x86)%"=="" (
+echo it's 32 bit captain
+SET PATH="C:\PROGRA~1\Borland\CBUILD~1\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32
+) else (
+echo 64 bits detected, captain
 SET PATH=C:\CBuilder5\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files (x86)\doxygen\bin";C:\Windows\System32
