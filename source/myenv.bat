c7ed62ef93fbc84785e9145d6ed085175d593fcd
Patrick Martin <patrickmmartin@gmail.com>
2012-03-11 21:23:53 +0000

    syntax was broken for 32-bit' simplified

--- source/myenv.bat
+++ source/myenv.bat
@@ -1,7 +1,3 @@
 @echo off
-IF "%ProgramFiles(x86)%"=="" (
-echo it's 32 bit captain
-SET PATH="C:\PROGRA~1\Borland\CBUILD~1\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32
-) else (
-echo 64 bits detected, captain
+IF "%ProgramFiles(x86)%"=="" SET PATH="C:\PROGRA~1\Borland\CBUILD~1\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32 & exit /b
 SET PATH=C:\CBuilder5\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files (x86)\doxygen\bin";C:\Windows\System32
