56ea3934b0d2ed69bbd7d5f25e67f225fbbb4a37
Patrick Martin <patrickmmartin@gmail.com>
2012-07-25 22:25:17 +0100

    updating to Juno build of Eclipse

--- source/eclipse.bat
+++ source/eclipse.bat
@@ -1,1 +1,2 @@
-C:\eclipse\eclipse-cpp-indigo-incubation-win32-x86_64\eclipse\eclipse.exe 
+:: C:\eclipse\eclipse-cpp-indigo-incubation-win32-x86_64\eclipse\eclipse.exe 
+C:\eclipse\eclipse-cpp-juno-RC3-win32-x86_64\eclipse\eclipse.exe
