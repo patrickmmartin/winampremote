ca370b97264a24316e749e7cc3e5f69b839d3da9
Patrick Martin <patrickmmartin@gmail.com>
2012-11-11 21:40:46 +0000

    concerning #110  * factoring out executor class to the end of ultimately removing duplication  ** removing the TestRPCServer class  ** implementing RPCExecutor class to handle RPC calls and hands off to instances for the actual winamp implementation and call events

--- source/RPCExecutor.h
+++ source/RPCExecutor.h
@@ -8,11 +8,8 @@
 #ifndef RPCSERVER_H_
 #define RPCSERVER_H_
 
-
 #include "IWinamp.h"
 #include "ICallObserver.h"
-
-#include "IRPCServer.h"
 
 namespace WinampRemote
 {
@@ -25,33 +22,51 @@ namespace Server
  * generated stubs, for generic behaviour, the correct handler needs to be
  * invoked for the mocked winamp or the actual winamp process
  */
-class RPCServer : public IRPCServer
+class RPCExecutor
 {
 private:
-	WinampRemote::Client::IWinamp * m_winamp;
+	WinampRemote::Server::IWinampServer * m_winamp;
 	WinampRemote::Remoting::ICallObserver * m_callObserver;
 public:
 	/**
-	 * constructor - requires a valid winamp handler and optional
-	 * observer for call events
-	 * @param winamp
-	 * @param callObserver
+	 * constructor
 	 */
-	RPCServer(WinampRemote::Client::IWinamp * winamp, WinampRemote::Remoting::ICallObserver * callObserver);
+	RPCExecutor();
 	/**
 	 * virtual destructor
 	 */
-	virtual ~RPCServer();
+	virtual ~RPCExecutor();
+
+	/**
+	 *  return the winamp server instance
+	 * @return the server instance
+	 */
+	virtual WinampRemote::Server::IWinampServer * getWinampServer();
+
+	/**
+	 * sets the winamp server implementation
+	 * @param winampServer
+	 */
+	virtual void setWinampServer(WinampRemote::Server::IWinampServer * winampServer);
+
+	/**
+	 * return the call observer instance
+	 * @return the observer
+	 */
+	virtual WinampRemote::Remoting::ICallObserver * getCallObserver();
 
 	/**
 	 * sets the call observer for the instance
 	 * @param callObserver
 	 */
 	virtual void setCallObserver(WinampRemote::Remoting::ICallObserver * callObserver);
+
 	/**
 	 * execute function - sets up and begins the main loop
 	 */
 	virtual void Execute();
+
+	static RPCExecutor& instance();
 
 };
 
--- source/testserver.cpp
+++ source/testserver.cpp
@@ -2,20 +2,26 @@
 
 #include <iostream>
 
-#include "TestRPCServer.h"
+#include "RPCExecutor.h"
 
+#include "WinampTestServer.h"
 #include "ConsoleCallObserver.h"
-
 
 #pragma argsused
 int main(int argc, char* argv[])
 {
 
-  ConsoleCallObserver cco;
+  WinampRemote::Remoting::ConsoleCallObserver cco;
+
+  // this code is only used in the out of process test process
+  WinampRemote::Server::WinampTestServer localWinamp;
+
   cco.notifyStatus("testserver starting");
 
   // for this mock server, no need for another thread, simply block on this
-  TTestRPCServer::Execute(cco);
+  WinampRemote::Server::RPCExecutor::instance().setWinampServer(&localWinamp);
+  WinampRemote::Server::RPCExecutor::instance().setCallObserver(&cco);
+  WinampRemote::Server::RPCExecutor::instance().Execute();
   cco.notifyStatus("RPC loop terminated: testserver exiting");
 
   return 0;
