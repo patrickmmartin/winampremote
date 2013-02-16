41a9d00172b28677cfae4a32d3b3d2ecf7e030c6
Patrick Martin <patrickmmartin@gmail.com>
2013-02-16 17:02:08 +0000

    closes #119 * more output to indicate the path taken * moved c:\WINDOWS\System32 to the start of the path

--- source/myenv.bat
+++ source/myenv.bat
@@ -1,3 +1,3 @@
 @echo off
-IF "%ProgramFiles(x86)%"=="" SET PATH="C:\PROGRA~1\Borland\CBUILD~1\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32 & exit /b
-SET PATH=C:\CBuilder5\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files\doxygen\bin";C:\Windows\System32
+IF "%ProgramFiles(x86)%"=="" echo setting 32-bit Windows path & SET PATH=C:\WINDOWS\System32;"C:\PROGRA~1\Borland\CBUILD~1\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin" & exit /b
+echo setting 64-bit Windows path & SET PATH=C:\WINDOWS\System32;C:\CBuilder5\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files\doxygen\bin"
