7abd92e0595e433a5d03546d642a833db14ca285
Patrick Martin <patrickmmartin@gmail.com>
2013-03-03 14:31:50 +0000

    resolves #148 updating to cater for lookup of framedyn.dll on 32-bit

--- source/myenv.bat
+++ source/myenv.bat
@@ -1,3 +1,3 @@
 @echo off
-IF "%ProgramFiles(x86)%"=="" echo setting 32-bit Windows path & SET PATH=C:\WINDOWS\System32;"C:\PROGRA~1\Borland\CBUILD~1\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin" & exit /b
+IF "%ProgramFiles(x86)%"=="" echo setting 32-bit Windows path & SET PATH=C:\WINDOWS\System32;C:\WINDOWS\System32\wbem;"C:\PROGRA~1\Borland\CBUILD~1\bin";"C:\Program Files\Git\cmd";"c:\Program Files\doxygen\bin" & exit /b
 echo setting 64-bit Windows path & SET PATH=C:\WINDOWS\System32;C:\CBuilder5\bin;"C:\Program Files (x86)\Git\cmd";"c:\Program Files\doxygen\bin"
